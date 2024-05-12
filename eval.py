7c63f0121da440903ba2fe88377811e5d897f46f
123 <55683566+wannabetter@users.noreply.github.com>
2024-05-12 14:08:01 +0800

    Delete eval.py

--- eval.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-
-if __name__ == '__main__':
-    YOLO = YOLO('VOC/Basketball/YOLOv8n_C2fDCN_C2f/weights/best.pt')
-    YOLO.val(data='voc.yaml', batch=32, workers=0, device='0')
