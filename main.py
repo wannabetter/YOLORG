c556ca0e6abf94f79a01eaf4fb040c4f9f0f8302
123 <55683566+wannabetter@users.noreply.github.com>
2024-05-12 14:08:08 +0800

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-if __name__ == '__main__':
-    YOLO = YOLO('VOC/YOLOv8n_C2D_Gold/weights/best.pt')
-    # YOLO.train(**{'cfg':'Yaml/Cfg/YOLOv8/YOLOv8n_VOC.yaml'})
-    YOLO.predict(r"heatmap/images/dog0.jpg", show=True,save=True, visualize=True)
